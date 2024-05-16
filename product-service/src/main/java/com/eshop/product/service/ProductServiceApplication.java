187d3a459c27460d7d813673f0e2428aaa51d03d
azzamaddouri <maddouriazza@gmail.com>
2024-05-16 13:41:32 +0200

    Configuration for Discovery Service Implementation using Eureka !

--- product-service/src/main/java/com/eshop/product/service/ProductServiceApplication.java
+++ product-service/src/main/java/com/eshop/product/service/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.eshop.product.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
