5c2aacd36a5c58b035c433b2b51606d5857a7f6c
azzamaddouri <maddouriazza@gmail.com>
2024-05-16 20:12:02 +0200

    All Microservices are secured by Keycloak authorization server!

---
+++ discovery-server/src/main/java/com/eshop/discovery/server/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.eshop.discovery.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/eureka/**"));
+        return httpSecurity.build();
+    }
+}
