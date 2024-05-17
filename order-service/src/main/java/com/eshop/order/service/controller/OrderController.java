de6ef44fdda71cfcc2db8cb116719dc36c314d78
azzamaddouri <maddouriazza@gmail.com>
2024-05-17 13:17:40 +0200

    Configured Circuit Breaker using  Resilience4J !

--- order-service/src/main/java/com/eshop/order/service/controller/OrderController.java
+++ order-service/src/main/java/com/eshop/order/service/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.eshop.order.service.controller;
 
 import com.eshop.order.service.dto.OrderRequest;
 import com.eshop.order.service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,8 +20,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-     public String PlaceOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="inventory")
+    @Retry(name="inventory")
+     public CompletableFuture<String> PlaceOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please order after some time!");
     }
 }
--- order-service/src/main/java/com/eshop/order/service/service/OrderService.java
+++ order-service/src/main/java/com/eshop/order/service/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
       Order order = new Order();
       order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
@@ -47,6 +47,7 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            return "Order Placed Successfully";
         } else{
             throw new IllegalArgumentException("Product is not in stock, please try again later !");
         }
